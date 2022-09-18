58d42649ae3a1ad868a340a740131278e7098e8b
sing3demons <sing3demons@live.com>
2022-09-18 16:22:53 +0700

    style:  templates x filters

--- app_foods/views.py
+++ app_foods/views.py
@@ -1,11 +1,21 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
 # Create your views here.
 all_foods = [
-    {'id': 1, 'title': 'Dark Choco Premium', 'price': 499, 'is_premium': True},
-    {'id': 2, 'title': 'Red Spicy', 'price': 499, 'is_premium': True},
-    {'id': 3, 'title': 'Blue Glacier', 'price': 349, 'is_premium': False},
+    {
+        'id': 1, 'title': 'Dark Choco Premium', 'price': 499,
+        'is_premium': True, 'promotion_end_at': datetime(2022, 2, 28)
+    },
+    {
+        'id': 2, 'title': 'Red Spicy', 'price': 499, 'is_premium': False,
+        'promotion_end_at': datetime(2022, 2, 15)
+    },
+    {
+        'id': 3, 'title': 'Blue Glacier', 'price': 349, 'is_premium': False,
+        'promotion_end_at': datetime(2022, 2, 15)
+    },
 ]
 
 
