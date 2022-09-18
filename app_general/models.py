393ea6a2d8ed313a3124825056ca64176981e112
sing3demons <sing3demons@live.com>
2022-09-18 22:50:44 +0700

    feat: create app_general.models.py

--- app_general/models.py
+++ app_general/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Subscription(models.Model):
+    CONST_STATUS = [
+        ('unapproved', 'Unapproved'),
+        ('approved', 'Approved'),
+        ('banned', 'Banned'),
+    ]
+    name = models.CharField(max_length=60)
+    email = models.EmailField(max_length=60, unique=True)
+    status = models.CharField(
+        max_length=15, choices=CONST_STATUS, default='unapproved')
+    register_at = models.DateTimeField(auto_now_add=True)
